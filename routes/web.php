af8822da29abb40761ec6ff9d1a55688d2dd92ce
osomreh23 <edsanuchiha@gmail.com>
2022-11-08 17:22:45 +0800

    working on starting goalies version1

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 Route::get('/line-combos',  [App\Http\Controllers\HockeyController::class, 'lineCombos']);
 Route::get('/todays-games',  [App\Http\Controllers\HockeyController::class, 'todaysGame']);
 Route::get('/news-feed',  [App\Http\Controllers\HockeyController::class, 'newsFeed']);
+Route::get('/starting-goaliesv1',  [App\Http\Controllers\HockeyController::class, 'startingGoaliesv1']);
 Route::get('/starting-goalies', [App\Http\Controllers\HockeyController::class, 'startingGoalies'])->name('starting-goalies');
 // Route::get('/hockey',  'HockeyController@index');
 
